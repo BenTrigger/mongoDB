e29e6cdeb7e32bce5fa9451d40ed2f3b917b48c1
Ben <benlerer@hotmail.com>
2020-03-22 13:05:05 +0200

    inser and find by dates, define events and insert as __dict__

---
+++ event.py
@@ -0,0 +1,13 @@
+import datetime
+
+class Event:
+    count = 1
+    def __init__(self):
+        self.date = datetime.datetime.now()
+        self.name = 'class insert' + str(Event.count)
+        self.files = []
+        Event.count += 1
+
+    def set__file_path(self, path="../out/", file_name="empty"):
+        self.files.append(path + file_name)
+        return self
--- mongoPy.py
+++ mongoPy.py
@@ -1,9 +1,28 @@
 from pymongo import MongoClient
+from event import Event
+
+import datetime
 
 client = MongoClient('localhost', 27017)
 db = client.test
-collectinT = db.people
+collectinT = db.events
 
-for val in collectinT.find({}):
-    print(val['firstname'])
+
+# eventShow = ({
+#     'name': "example3",
+#     'created_date': datetime.datetime.now()
+# })
+#
+for i in range(4):
+    collectinT.insert_one(Event().set__file_path(file_name="ben").__dict__)
+
+# from_date = datetime.datetime(2010, 12, 31, 12, 30, 30, 125000)
+# to_date = datetime.datetime(2020, 12, 31, 12, 30, 30, 125000)
+# for event in collectinT.find({"created_date": {"$gte": from_date, "$lt": to_date}}):
+#     print(event['created_date'])
+
+
+# for val in collectinT.find({}):
+#     print(val['firstname'])
+
 
